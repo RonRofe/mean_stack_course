2964f0a2641b9c8fedc67d37360e217051cd638f
rapitec <ron@rapitec.co.il>
2019-11-22 19:39:03 +0200

    list of posts

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent {
+    // public posts: {title: string, content: string}[] = [
+    //     {title: 'First Post', content: 'This is the first post\'s content'},
+    //     {title: 'First Post', content: 'This is the first post\'s content'},
+    //     {title: 'First Post', content: 'This is the first post\'s content'}
+    // ];
+
+    public posts: {title: string, content: string}[] = [];
+
+    public get showPosts(): boolean {
+        return this.posts.length > 0;
+    }
+}
