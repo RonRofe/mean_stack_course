e6eaafdf2e941cd4c31d08cd721058af471a0717
rapitec <ron@rapitec.co.il>
2019-12-01 15:21:50 +0200

    creating user model

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const { router: postsRoutes } = require('./routes/posts');
+const { router: userRoutes } = require('./routes/user');
 
 const app = express();
 
@@ -33,5 +34,6 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/posts', postsRoutes);
+app.use('/api/user', postsRoutes);
 
 module.exports = app;
